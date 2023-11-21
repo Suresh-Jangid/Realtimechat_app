6bd55042aa23aff984b538e517d80323aea45edf
suresh jangid <sureshjangid737@gmail.com>
2023-11-21 09:12:43 +0530

    header and chat box commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ChatBoxComponent
   ],
   imports: [
     BrowserModule,
